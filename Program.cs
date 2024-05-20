efaed1aa3305503df0ca8390113270b8c8e17f46
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-05-20 17:31:17 -0300

    special-topics

--- Program.cs
+++ Program.cs
@@ -6,9 +6,30 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            double preco = 34.5;
-            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
-            System.Console.WriteLine(desconto);
+            string original = "abcde FGHIJ ABC abc DEFG ";
+            string s1 = original.ToUpper();
+            string s2 = original.ToLower();
+            string s3 = original.Trim(); //começo e fim
+            int n1 = original.IndexOf("bc"); //primeira ocorrencia
+            int n2 = original.LastIndexOf("bc"); //ultima ocorrencia
+            string s4 = original.Substring(3); //recortar
+            string s5 = original.Substring(3, 5); 
+            string s6 = original.Replace('a', 'x'); //substituir
+            string s7 = original.Replace("abc", "xy");
+            bool b1 = String.IsNullOrEmpty(original); //conteudo nulo ou vazio
+            bool b2 = String.IsNullOrWhiteSpace(original); //nulo ou espaço em branco 
+            Console.WriteLine("Original: -" + original + "-");
+            Console.WriteLine("ToUpper: -" + s1 + "-");
+            Console.WriteLine("ToLower: -" + s2 + "-");
+            Console.WriteLine("Trim: -" + s3 + "-");
+            Console.WriteLine("IndexOf('bc'): " + n1);
+            Console.WriteLine("LastIndexOf('bc'): " + n2);
+            Console.WriteLine("Substring(3): -" + s4 + "-");
+            Console.WriteLine("Substring(3, 5): -" + s5 + "-");
+            Console.WriteLine("Replace('a', 'x'): -" + s6 + "-");
+            Console.WriteLine("Replace('abc', 'xy'): -" + s7 + "-");
+            Console.WriteLine("IsNullOrEmpty: " + b1);
+            Console.WriteLine("IsNullOrWhiteSpace: " + b2);
         }
     }
 }
