f1ce68151796422bf9e368e0e59eb29b6809f97f
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-05-20 17:46:32 -0300

    special-topics

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,30 +7,46 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            string original = "abcde FGHIJ ABC abc DEFG ";
-            string s1 = original.ToUpper();
-            string s2 = original.ToLower();
-            string s3 = original.Trim(); //começo e fim
-            int n1 = original.IndexOf("bc"); //primeira ocorrencia
-            int n2 = original.LastIndexOf("bc"); //ultima ocorrencia
-            string s4 = original.Substring(3); //recortar
-            string s5 = original.Substring(3, 5); 
-            string s6 = original.Replace('a', 'x'); //substituir
-            string s7 = original.Replace("abc", "xy");
-            bool b1 = String.IsNullOrEmpty(original); //conteudo nulo ou vazio
-            bool b2 = String.IsNullOrWhiteSpace(original); //nulo ou espaço em branco 
-            Console.WriteLine("Original: -" + original + "-");
-            Console.WriteLine("ToUpper: -" + s1 + "-");
-            Console.WriteLine("ToLower: -" + s2 + "-");
-            Console.WriteLine("Trim: -" + s3 + "-");
-            Console.WriteLine("IndexOf('bc'): " + n1);
-            Console.WriteLine("LastIndexOf('bc'): " + n2);
-            Console.WriteLine("Substring(3): -" + s4 + "-");
-            Console.WriteLine("Substring(3, 5): -" + s5 + "-");
-            Console.WriteLine("Replace('a', 'x'): -" + s6 + "-");
-            Console.WriteLine("Replace('abc', 'xy'): -" + s7 + "-");
-            Console.WriteLine("IsNullOrEmpty: " + b1);
-            Console.WriteLine("IsNullOrWhiteSpace: " + b2);
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks); //base para datetimenow
+
+            DateTime d2 = new DateTime();
+
+            // DateTime(ano, mes, dia)
+            // DateTime(ano, mes, dia, hora, minuto, segundo)
+            // DateTime(ano, mes, dia, hora, minuto, segundo, milissegundo)
+
+            DateTime a1 = new DateTime(2000, 8, 15);
+            DateTime b2 = new DateTime(2000, 8, 15, 13, 5, 58);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58, 275);
+            Console.WriteLine(a1);
+            Console.WriteLine(b2);
+            Console.WriteLine(d3);
+
+            DateTime m1 = DateTime.Now;
+            DateTime m2 = DateTime.UtcNow;
+            DateTime m3 = DateTime.Today;
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            Console.WriteLine(m3);
+
+            DateTime n1 = DateTime.Parse("2000-08-15");
+            DateTime n2 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime n3 = DateTime.Parse("15/08/2000");
+            DateTime n4 = DateTime.Parse("15/08/2000 13:05:58");
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+
+
+            DateTime e1 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd",
+CultureInfo.InvariantCulture);
+            DateTime e2 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss",
+            CultureInfo.InvariantCulture);
+            Console.WriteLine(e1);
+            Console.WriteLine(e2);
         }
     }
 }
