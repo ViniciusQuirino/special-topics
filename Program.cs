4874cd0fb3553ce55900b624b354da81bca72907
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-05-20 16:26:17 -0300

    special-topics

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = int.Parse(Console.ReadLine());
+            string day;
+            switch (x)
+            {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Wednesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+            }
+            Console.WriteLine("Day: " + day);
         }
     }
 }
