e4b0eeeff02213732d3dec02484ef6c25c006f3f
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-05-21 10:46:21 -0300

    special-topics

--- Program.cs
+++ Program.cs
@@ -7,33 +7,58 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            TimeSpan t1 = new TimeSpan(0, 1, 30);
-            Console.WriteLine(t1);
-            Console.WriteLine(t1.Ticks);
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day: " + d.Day);
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year);
 
-            TimeSpan t2 = new TimeSpan(900000000L);
-            Console.WriteLine(t2);
+            Console.WriteLine();
+            //FORMATAÇÃO
+            string s1 = d.ToLongDateString();
+            string s2 = d.ToLongTimeString();
+            string s3 = d.ToShortDateString();
+            string s4 = d.ToShortTimeString();
+            string s5 = d.ToString();
+            string s6 = d.ToString("yyyy-MM-dd HH:mm:ss");
+            string s7 = d.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
 
-            TimeSpan ts1 = new TimeSpan(1, 2, 30); // 1 hora, 2 minutos, 30 segundos
-            TimeSpan ts2 = new TimeSpan(2, 5, 30, 40); // 2 dias, 5 horas, 30 minutos, 40 segundos
-            TimeSpan ts3 = new TimeSpan(3, 4, 5, 6, 700); // 3 dias, 4 horas, 5 minutos, 6 segundos, 700 milissegundos
+            Console.WriteLine();
 
-            Console.WriteLine(ts1);
-            Console.WriteLine(ts2);
-            Console.WriteLine(ts3.Ticks);
+            DateTime x = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+            // DateTime y2 = x.Add(timeSpan);
+            DateTime y3 = x.AddDays(3);
+            DateTime y4 = x.AddHours(2);
+            // DateTime y5 = x.AddMilliseconds(double);
+            // DateTime y6 = x.AddMinutes(double);
+            // DateTime y7 = x.AddMonths(int);
+            // DateTime y8 = x.AddSeconds(double);
+            // DateTime y9 = x.AddTicks(long);
+            // DateTime y10 = x.AddYears(int);
+            // DateTime y11 = x.Subtract(timeSpan);
+            TimeSpan t12 = x.Subtract(y4);
 
-            TimeSpan a1 = TimeSpan.FromDays(1.5); //um dia e meio
-            TimeSpan a2 = TimeSpan.FromHours(1.5); //uma hora e meia
-            TimeSpan a3 = TimeSpan.FromMinutes(1.5);
-            TimeSpan a4 = TimeSpan.FromSeconds(1.5);
-            TimeSpan a5 = TimeSpan.FromMilliseconds(1.5);
-            TimeSpan a6 = TimeSpan.FromTicks(900000000L); //apenas inteiros
-            Console.WriteLine(a1);
-            Console.WriteLine(a2);
-            Console.WriteLine(a3);
-            Console.WriteLine(a4);
-            Console.WriteLine(a5);
-            Console.WriteLine(a6);
+            Console.WriteLine(x);
+            Console.WriteLine(y3);
+            Console.WriteLine(t12);
+
         }
     }
 }
